f18b7175a9d176a665c58b741684b6492ad083e8
Grant Imbo <grant.imbo@gmail.com>
2020-05-08 03:18:22 +0800

    layout updates

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 // sidebar toggle
-
-
 $('button.sidebar-toggle').on('click', function() {
 
     let sideBarToggle = $('button.sidebar-toggle i');
@@ -15,4 +13,58 @@ $('button.sidebar-toggle').on('click', function() {
     // toggle classes
     $('main aside').toggleClass('hide');
     $('main section').toggleClass('full');
+
+    // inventory analysis
+    $('.table-responsive.inventory-analysis .hidden-fields').toggleClass('active');
 });
+
+// steps 
+$('.steps .step').on('click', function(){
+
+    let current = $(this).attr('data-steps'),
+        e = $(this).addClass('active');
+
+    switch (current) {
+        case "2":
+            e.siblings().removeClass('active');
+            e.siblings('.step.s1').addClass('active');
+            e.siblings('.step.s2').addClass('active');
+            e.siblings('.progress').find('.progress-bar').attr('style', 'width: 20%');
+            break;
+        case "3":
+            e.siblings().removeClass('active');
+            e.siblings('.step.s1').addClass('active');
+            e.siblings('.step.s2').addClass('active');
+            e.siblings('.step.s3').addClass('active');
+            e.siblings('.progress').find('.progress-bar').attr('style', 'width: 40%');
+            break;
+        case "4":
+            e.siblings().removeClass('active');
+            e.siblings('.step.s1').addClass('active');
+            e.siblings('.step.s2').addClass('active');
+            e.siblings('.step.s3').addClass('active');
+            e.siblings('.step.s4').addClass('active');
+            e.siblings('.progress').find('.progress-bar').attr('style', 'width: 60%');
+            break;
+        case "5":
+            e.siblings().removeClass('active');
+            e.siblings('.step.s1').addClass('active');
+            e.siblings('.step.s2').addClass('active');
+            e.siblings('.step.s3').addClass('active');
+            e.siblings('.step.s4').addClass('active');
+            e.siblings('.step.s5').addClass('active');
+            e.siblings('.progress').find('.progress-bar').attr('style', 'width: 80%');
+            break;
+        case "6":
+            e.siblings().addClass('active');
+            e.siblings('.progress').find('.progress-bar').attr('style', 'width: 100%');
+            break;
+        default:
+            e.siblings().removeClass('active');
+            e.siblings('.step.s1').addClass('active');
+            e.siblings('.progress').find('.progress-bar').attr('style', 'width: 0');
+    }
+})
+
+// tooltip init
+$('[data-toggle="tooltip"]').tooltip();
