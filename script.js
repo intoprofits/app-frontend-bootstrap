829559ca4352963ecb5b9a9187792e00161d887f
Grant Imbo <grant.imbo@gmail.com>
2020-05-08 21:01:01 +0800

    order tracking drop file

--- script.js
+++ script.js
@@ -29,14 +29,14 @@ $('.steps .step').on('click', function(){
             e.siblings().removeClass('active');
             e.siblings('.step.s1').addClass('active');
             e.siblings('.step.s2').addClass('active');
-            e.siblings('.progress').find('.progress-bar').attr('style', 'width: 20%');
+            e.siblings('.progress').find('.progress-bar').attr('style', 'width: 17%');
             break;
         case "3":
             e.siblings().removeClass('active');
             e.siblings('.step.s1').addClass('active');
             e.siblings('.step.s2').addClass('active');
             e.siblings('.step.s3').addClass('active');
-            e.siblings('.progress').find('.progress-bar').attr('style', 'width: 40%');
+            e.siblings('.progress').find('.progress-bar').attr('style', 'width: 34%');
             break;
         case "4":
             e.siblings().removeClass('active');
@@ -44,7 +44,7 @@ $('.steps .step').on('click', function(){
             e.siblings('.step.s2').addClass('active');
             e.siblings('.step.s3').addClass('active');
             e.siblings('.step.s4').addClass('active');
-            e.siblings('.progress').find('.progress-bar').attr('style', 'width: 60%');
+            e.siblings('.progress').find('.progress-bar').attr('style', 'width: 51%');
             break;
         case "5":
             e.siblings().removeClass('active');
@@ -53,9 +53,19 @@ $('.steps .step').on('click', function(){
             e.siblings('.step.s3').addClass('active');
             e.siblings('.step.s4').addClass('active');
             e.siblings('.step.s5').addClass('active');
-            e.siblings('.progress').find('.progress-bar').attr('style', 'width: 80%');
+            e.siblings('.progress').find('.progress-bar').attr('style', 'width: 67%');
             break;
         case "6":
+            e.siblings().removeClass('active');
+            e.siblings('.step.s1').addClass('active');
+            e.siblings('.step.s2').addClass('active');
+            e.siblings('.step.s3').addClass('active');
+            e.siblings('.step.s4').addClass('active');
+            e.siblings('.step.s5').addClass('active');
+            e.siblings('.step.s6').addClass('active');
+            e.siblings('.progress').find('.progress-bar').attr('style', 'width: 83%');
+            break;
+        case "7":
             e.siblings().addClass('active');
             e.siblings('.progress').find('.progress-bar').attr('style', 'width: 100%');
             break;
