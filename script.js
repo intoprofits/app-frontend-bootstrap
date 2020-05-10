a5fc62cd428995a6d879d36a7774483facd8472a
Grant Imbo <grant.imbo@gmail.com>
2020-05-10 21:25:18 +0800

    PO creator input validations

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+// Author: @grantimbo
+// Author URI: grantimbo.com
+
+
+// ----------------
+// general
+// ----------------
+
 // sidebar toggle
-$('button.sidebar-toggle').on('click', function() {
+$('button.sidebar-toggle').click(function() {
 
     let sideBarToggle = $('button.sidebar-toggle i');
 
@@ -18,8 +26,13 @@ $('button.sidebar-toggle').on('click', function() {
     $('.table-responsive.inventory-analysis .hidden-fields').toggleClass('active');
 });
 
-// steps 
-$('.steps .step').on('click', function(){
+
+// ----------------
+// order tracking
+// ----------------
+
+// dot steps 
+$('.steps .step').click(function(){
 
     let current = $(this).attr('data-steps'),
         e = $(this).addClass('active');
@@ -78,3 +91,73 @@ $('.steps .step').on('click', function(){
 
 // tooltip init
 $('[data-toggle="tooltip"]').tooltip();
+
+
+
+// ----------------
+// po file modal
+// ----------------
+
+// PO select inputs
+$(".POCreator select").change(function(e){
+
+    // choose template - english or chinese
+    if (e.target.id == 'template') {
+        let POFile = $("#addPO table.pofile");
+
+        if ($(this)[0].value != "English") {
+            POFile.addClass('ch');
+            POFile.removeClass('en');
+        } else {
+            POFile.removeClass('ch');
+            POFile.addClass('en');
+        }
+
+    // else set value to PO modal values
+    } else {
+        let PONode = e.target.attributes.id.nodeValue;
+        element = 'input#PO_' + PONode;
+
+        $(element)[0].value = e.target.value;
+    }
+
+    $(".reviewpo.alert").addClass("d-none");
+    $(this).removeClass('is-invalid');
+});
+
+// PO text inputs
+$(".POCreator input").keyup(function(e){
+    $(".reviewpo.alert").addClass("d-none");
+    $(this).removeClass('is-invalid');
+    let element = 'input#PO_' + e.target.id;
+    console.log($(element)[0].value);
+});
+
+// review button
+$("#reviewPO").click(function(e){
+
+    let POinputs = [
+        $(".POCreator select#template")[0],
+        $(".POCreator select#supplier")[0],
+        $(".POCreator select#shipAddress")[0],
+        $(".POCreator select#shipMethod")[0],
+        $(".POCreator select#exportTerm")[0],
+        $(".POCreator select#product")[0],
+        $(".POCreator input#quantity")[0],
+        $(".POCreator input#unitCost")[0],
+        $(".POCreator input#discount")[0]
+    ]
+
+    POinputs.forEach(element => {
+
+        if (element.value == "") {
+            $(element).addClass('is-invalid');
+            $(".reviewpo.alert").removeClass("d-none");
+            $(this).removeAttr("data-toggle");
+        } else {
+            $(element).removeClass('is-invalid');
+            $(".reviewpo.alert").addClass("d-none");
+            this.setAttribute("data-toggle", "modal");
+        }
+    });
+});
