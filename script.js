be3d6ad91ea0d6fe088078a0e88b5c79da4c7a99
Grant Imbo <grant.imbo@gmail.com>
2020-05-11 13:07:06 +0800

    optimize 'dot steps'

--- script.js
+++ script.js
@@ -35,56 +35,31 @@ $('button.sidebar-toggle').click(function() {
 $('.steps .step').click(function(){
 
     let current = $(this).attr('data-steps'),
-        e = $(this).addClass('active');
+        e = $(this);
+    
+    e.addClass('active').siblings().removeClass('active');
+    e.prevAll().addClass('active');
 
     switch (current) {
         case "2":
-            e.siblings().removeClass('active');
-            e.siblings('.step.s1').addClass('active');
-            e.siblings('.step.s2').addClass('active');
             e.siblings('.progress').find('.progress-bar').attr('style', 'width: 17%');
             break;
         case "3":
-            e.siblings().removeClass('active');
-            e.siblings('.step.s1').addClass('active');
-            e.siblings('.step.s2').addClass('active');
-            e.siblings('.step.s3').addClass('active');
             e.siblings('.progress').find('.progress-bar').attr('style', 'width: 34%');
             break;
         case "4":
-            e.siblings().removeClass('active');
-            e.siblings('.step.s1').addClass('active');
-            e.siblings('.step.s2').addClass('active');
-            e.siblings('.step.s3').addClass('active');
-            e.siblings('.step.s4').addClass('active');
             e.siblings('.progress').find('.progress-bar').attr('style', 'width: 51%');
             break;
         case "5":
-            e.siblings().removeClass('active');
-            e.siblings('.step.s1').addClass('active');
-            e.siblings('.step.s2').addClass('active');
-            e.siblings('.step.s3').addClass('active');
-            e.siblings('.step.s4').addClass('active');
-            e.siblings('.step.s5').addClass('active');
             e.siblings('.progress').find('.progress-bar').attr('style', 'width: 67%');
             break;
         case "6":
-            e.siblings().removeClass('active');
-            e.siblings('.step.s1').addClass('active');
-            e.siblings('.step.s2').addClass('active');
-            e.siblings('.step.s3').addClass('active');
-            e.siblings('.step.s4').addClass('active');
-            e.siblings('.step.s5').addClass('active');
-            e.siblings('.step.s6').addClass('active');
             e.siblings('.progress').find('.progress-bar').attr('style', 'width: 83%');
             break;
         case "7":
-            e.siblings().addClass('active');
             e.siblings('.progress').find('.progress-bar').attr('style', 'width: 100%');
             break;
         default:
-            e.siblings().removeClass('active');
-            e.siblings('.step.s1').addClass('active');
             e.siblings('.progress').find('.progress-bar').attr('style', 'width: 0');
     }
 })
